d16c1cedf5f01094c9c62f7b0788bef42e55804a
Kelvin Muza <kelvinm@hyperiondev.com>
2023-08-11 23:58:12 +0200

    update test db

--- core/settings.py
+++ core/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -83,6 +84,9 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+if 'TEST' in os.environ:
+    DATABASES['default']['HOST'] = 'localhost'
 
 
 # Password validation
